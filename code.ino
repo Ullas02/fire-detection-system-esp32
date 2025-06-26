eb23b0fcab4bcf3d626448bc9882b23051a5fd49
Ullas02 <166708206+Ullas02@users.noreply.github.com>
2025-06-26 19:57:58 +0530

    code.ino

--- code.ino
+++ code.ino
@@ -1,37 +1,37 @@
-#define BLYNK_TEMPLATE_ID "TMPLkOtFt5y-"
-#define BLYNK_TEMPLATE_NAME "fdc98"
-#define BLYNK_AUTH_TOKEN "WyHXrQ1dtwD70iYLRCqTSvcKJ-OYpCC2"
+#define BLYNK_TEMPLATE_ID "YOUR_BLYNK_TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "YOUR_BLYNK_TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "YOUR_BLYNK_AUTH_TOKEN"
 #define BLYNK_PRINT Serial
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-char auth[] = "WyHXrQ1dtwD70iYLRCqTSvcKJ-OYpCC2";
-char ssid[] = "abcd";
-char pass[] = "zxcvbnm11";
+char auth[] = "YOUR_BLYNK_TEMPLATE_NAME";
+char ssid[] = "YOUR_SSID";
+char pass[] = "YOUR_WIFI_PASSWORD";
 
 BlynkTimer timer;
 
-#define fire 18
-#define buzzer  14
+#define fire 18  // connected fire sensor pin number to ESP-32
+#define buzzer  14  // connected buzzer pin number to ESP-32
 int fire_Val = 0;
 
 WidgetLED led(V1);
 
-void setup() //Setup function - only function that is run in deep sleep mode
+void setup() 
 {
-  Serial.begin(9600); //Start the serial output at 9600 baud
+  Serial.begin(9600); 
   pinMode(fire, INPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(19, OUTPUT);
   pinMode(2, OUTPUT);
-  Blynk.begin(auth, ssid, pass);//Splash screen delay
+  Blynk.begin(auth, ssid, pass); 
   delay(2000);
   timer.setInterval(500L, mySensor);
 }
 
-void loop() //Loop function
+void loop() 
 {
   Blynk.run();
   timer.run();
@@ -47,7 +47,7 @@ void mySensor()
     digitalWrite(19, LOW);
     Blynk.virtualWrite(V0, 1);
     Blynk.notify("Fire detected!");
-    Blynk.email("9663890904u@gmail.com","Alert","Fire detected!!");
+    Blynk.email("YOUR_EMAIL@email.com","Alert","Fire detected!!");
     Blynk.logEvent("fire_alert");
     Serial.println("FIRE DETECTED!!");
     led.on();
